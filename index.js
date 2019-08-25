f471ea26de84c53b88519b3ba540afb3a7c4a89c
brad-bartlett <bradbartlett222@gmail.com>
2019-08-25 17:18:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const append = ["Milo", "Otis", "Garfield"];
-const prepend = ["Milo", "Otis", "Garfield"];
-const removeLast = ["Milo", "Otis", "Garfield"];
-const removeFirst = ["Milo", "Otis", "Garfield"];
-
 append.push("Odie");
+const prepend = ["Milo", "Otis", "Garfield"];
 prepend.unishft("Odie");
+const removeLast = ["Milo", "Otis", "Garfield"];
 removeLast.pop();
+const removeFirst = ["Milo", "Otis", "Garfield"];
 removeFirst.shift();
